2b4f438db14e1a0a3005f1f2e758209685d65c5a
hackinzoomer0 <163362751+hackinzoomer0@users.noreply.github.com>
2024-03-13 19:37:02 -0500

    Update content.js

--- content.js
+++ content.js
@@ -2,7 +2,7 @@ document.querySelectorAll('img').forEach(img => {
     fetch(`https://api.moderatecontent.com/moderate/?key=232fab016369c0a78f449a173a5fd64b&url=` + img.src).then(res => { //Yeah my API key is exposed. It's a free key, you can steal it if you want I guess I'll just make another
         if (res.ok) {
             res.json().then(rating => {
-                if (rating.predictions.adult > 1){
+                if (rating.predictions.adult > 20){
                     img.style.filter = 'blur(8px)'
                 }
             })
